82506112f74300b734b5b8e0ca41e34583e3d988
YuYoHan <zxzz8014@naver.com>
2024-09-11 15:30:51 +0900

    [Study] modelMapper 공부

--- backend_mall/src/main/java/com/example/backend_mall/service/TodoServiceImpl.java
+++ backend_mall/src/main/java/com/example/backend_mall/service/TodoServiceImpl.java
@@ -1,18 +1,27 @@
 package com.example.backend_mall.service;
 
 import com.example.backend_mall.dto.TodoDTO;
+import com.example.backend_mall.entity.TodoEntity;
+import com.example.backend_mall.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @Log4j2
+@RequiredArgsConstructor
 public class TodoServiceImpl implements TodoService{
+
+    private final ModelMapper modelMapper;
+    private final TodoRepository todoRepository;
 
     @Override
     public Long register(TodoDTO todo) {
-        log.info("-----------");
-        return null;
+        TodoEntity todoEntity = modelMapper.map(todo, TodoEntity.class);
+        TodoEntity savedTodo = todoRepository.save(todoEntity);
+        return savedTodo.getTno();
     }
 }
