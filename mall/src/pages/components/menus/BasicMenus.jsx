b656e18000bf35ec45b236a166874802dc2e8af0
YuYoHan <zxzz8014@naver.com>
2025-03-03 17:37:56 +0900

    [Study] 코드로 배우는 리액트

--- mall/src/pages/components/menus/BasicMenus.jsx
+++ mall/src/pages/components/menus/BasicMenus.jsx
@@ -11,6 +11,9 @@ const BasicMenu = () => {
           <li className="pr-6 text-2xl">
             <Link to={"/about"}>About</Link>
           </li>
+          <li className="pr-6 text-2xl">
+            <Link to={'/todo/'}>Todo</Link>
+          </li>
         </ul>
       </div>
       <div className="w-1/5 flex justify-end bg-orange-300 p-4 font-medium">
