ad22b859382bd9914c6c849cfb7309d3e1c89c54
YuYoHan <zxzz8014@naver.com>
2025-03-06 14:32:31 +0900

    [Add] useNavigate() 추가

--- mall/src/pages/todo/Index.jsx
+++ mall/src/pages/todo/Index.jsx
@@ -1,20 +1,39 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import BasicLayout from "../../layouts/BasicLayout";
+import { useCallback } from "react";
 
 const IndexPage = () => {
-  <BasicLayout>
-    <div className="w-full flex m-2 p-2">
-      <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline">
-        List
-      </div>
-      <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline">
-        ADD
-      </div>
-      <div className="flex flex-wrap w-full">
-        <Outlet />
-      </div>
-    </div>
-  </BasicLayout>;
+    const navigate = useNavigate();
+
+    const handleClickList = useCallback(() => {
+        navigate({ pathname: "list" });
+    });
+
+    const handleClickAdd = useCallback(() => {
+        navigate({ pathname: "add" });
+    });
+
+    return (
+        <BasicLayout>
+            <div className="w-full flex m-2 p-2">
+                <div
+                    className="text-xl m-1 p-2 w-20 font-extrabold text-center underline"
+                    onClick={handleClickList}
+                >
+                    List
+                </div>
+                <div
+                    className="text-xl m-1 p-2 w-20 font-extrabold text-center underline"
+                    onClick={handleClickAdd}
+                >
+                    ADD
+                </div>
+                <div className="flex flex-wrap w-full">
+                    <Outlet />
+                </div>
+            </div>
+        </BasicLayout>
+    );
 };
 
 export default IndexPage;
