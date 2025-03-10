1464b8b590b246b962f892b437f3ead361fed2e2
YuYoHan <zxzz8014@naver.com>
2025-03-10 19:59:08 +0900

    [Modify] fomatter 수정

--- backend_mall/src/main/java/com/example/backend_mall/config/formatter/LocalDateFormatter.java
+++ backend_mall/src/main/java/com/example/backend_mall/config/formatter/LocalDateFormatter.java
@@ -12,11 +12,11 @@ public class LocalDateFormatter implements Formatter<LocalDateTime> {
 
     @Override
     public LocalDateTime parse(String text, Locale locale) throws ParseException {
-        return LocalDateTime.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        return LocalDateTime.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
     @Override
     public String print(LocalDateTime object, Locale locale) {
-        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(object);
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd").format(object);
     }
 }
--- backend_mall/src/main/java/com/example/backend_mall/config/security/SecurityConfig.java
+++ backend_mall/src/main/java/com/example/backend_mall/config/security/SecurityConfig.java
@@ -5,6 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -12,6 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
@@ -20,5 +28,21 @@ public class SecurityConfig {
 
 
         return http.build();
+    }
+
+    // CORS에 대한 설정
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // Spring Boot 2.4+부터는 setAllowedOrigins 대신 setAllowedOriginPatterns를 사용하는 것이 권장
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*")); // 허용할 Origin
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 메서드
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // 허용할 헤더
+        configuration.setAllowCredentials(true); // 인증 정보 허용
+        configuration.setExposedHeaders(Arrays.asList("Cache-Control", "Content-Type", "X-Accel-Buffering","Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정
+        return source;
     }
 }
