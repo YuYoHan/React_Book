1863aa5261dfc5cc7987c8639835bef0b2260170
YuYoHan <zxzz8014@naver.com>
2025-03-10 22:17:41 +0900

    [Add] 목록 데이터 가져오기

---
+++ mall/src/pages/todo/ListComponent.jsx
@@ -0,0 +1,61 @@
+// ListComponent는 useCustomMove()를 이용해서 현재 경로의 page와 size를 구성하고
+// API서버를 호출합니다. 서버의 데이터는 dtoList라는 배열 데이터와 pageNumList라는
+// 페이지 번호들이 존재하고, 이전(prev)/다음(next) 등의 추가적인 데이터가 있습니다.
+import { useEffect, useState } from "react";
+import { getList } from "../../api/todoApi";
+import useCustomMove from "../../hooks/useCustomMove";
+
+const initState = {
+    dtoList: [],
+    pageNumeList: [],
+    pageRequestDTO: null,
+    prev: false,
+    next: false,
+    tototalCount: 0,
+    prevPage: 0,
+    nextPage: 0,
+    totalPage: 0,
+    current: 0,
+};
+
+const ListComponent = () => {
+    const { page, size } = useCustomMove();
+
+    const [serverData, setServerData] = useState(initState);
+
+    useEffect(() => {
+        getList({ page, size }).then((data) => {
+            console.log(data);
+            setServerData(data);
+        });
+    }, [page, size]);
+
+    return (
+        <>
+            <div className="border-2 border-blue-100 mt-10 mr-2 ml-2">
+                <div className="flex flex-wrap mx-auto justify-center p-6">
+                    {serverData.dtoList.map((todo) => {
+                        <div
+                            key={todo.tno}
+                            className="w-full min-w-[400px] p-2 m-2 rounded shadow-md"
+                        >
+                            <div className="flex">
+                                <div className="font-extrabold text-2xl p-2 w-1/12">
+                                    {todo.tno}
+                                </div>
+                                <div className="text-1xl m-1 p-2 w-8/12 font-extrabold">
+                                    {todo.title}
+                                </div>
+                                <div className="text-1xl m-1 p-2 w-2/10 font-medium">
+                                    {todo.dueDate}
+                                </div>
+                            </div>
+                        </div>;
+                    })}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ListComponent;
--- mall/src/pages/todo/ReadPage.jsx
+++ mall/src/pages/todo/ReadPage.jsx
@@ -1,37 +1,10 @@
-import { useCallback } from "react";
-import {
-    createSearchParams,
-    useNavigate,
-    useParams,
-    useSearchParams,
-} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import ReadComponent from "./ReadComponent";
 
 const ReadPage = () => {
     // useParams()를 이용해서 지정된 변수를 추출할 수 있다.
     // 경로 자체 값을 사용
     const { tno } = useParams();
-    const navigate = useNavigate();
-    const [queryParams] = useSearchParams();
-    const page = queryParams.get("page")
-        ? parseInt(queryParams.get("page"))
-        : 1;
-    const size = queryParams.get("size")
-        ? parseInt(queryParams.get("size"))
-        : 10;
-
-    const queryStr = createSearchParams({ page, size }).toString();
-
-    const moveToModify = useCallback(
-        (tno) => {
-            navigate({ pathname: `/todo/modify/${tno}`, search: queryStr });
-        },
-        [tno, page, size]
-    );
-
-    const moveToList = useCallback(() => {
-        navigate({ pathname: `/todo/list`, search: queryStr });
-    }, [page, size]);
 
     return (
         <>
