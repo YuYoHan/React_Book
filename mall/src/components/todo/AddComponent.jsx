12f8127b2a80d3b973a30a63e81eb02921be61eb
YuYoHan <zxzz8014@naver.com>
2025-03-11 20:41:19 +0900

    [Add] 모달창 추가

--- mall/src/components/todo/AddComponent.jsx
+++ mall/src/components/todo/AddComponent.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { postAdd } from "../../api/todoApi";
+import ResultModal from "../common/ResultModal";
 
 const initState = {
     title: "",
@@ -9,6 +10,7 @@ const initState = {
 
 const AddComponent = () => {
     const [todo, setTodo] = useState({ ...initState });
+    const [result, setResult] = useState(null); // 결과 상태
 
     const handleChangeTodo = (e) => {
         todo[e.target.name] = e.target.value;
@@ -20,6 +22,7 @@ const AddComponent = () => {
         postAdd(todo)
             .then((result) => {
                 console.log(result);
+                setResult(result.TNO); // 결과 데이터 변경
                 setTodo({ ...initState });
             })
             .catch((e) => {
@@ -27,8 +30,22 @@ const AddComponent = () => {
             });
     };
 
+    const closeModal = () => {
+        setResult(null);
+    };
+
     return (
         <div className="border-2 border-sky-200 mt-10 m-2 p-4">
+            {/* 모달 처리 */}
+            {result ? (
+                <ResultModal
+                    title={"Add Result"}
+                    content={`New ${result} Added`}
+                    callbackFn={closeModal}
+                />
+            ) : (
+                <></>
+            )}
             <div className="flex justify-center">
                 <div className="relative mb-4 flex w-full flex-wrap items-stretch">
                     <div className="w-1/5 p-6 text-right font-bold">TITLE</div>
