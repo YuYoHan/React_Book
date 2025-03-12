a72a315075373b10fe8bce7a513baede7434c0f4
YuYoHan <zxzz8014@naver.com>
2025-03-13 07:20:36 +0900

    [Feat] 업로드 파일 보여주기 기능 구현

--- backend_mall/src/main/java/com/example/backend_mall/config/file/CustomFileUtil.java
+++ backend_mall/src/main/java/com/example/backend_mall/config/file/CustomFileUtil.java
@@ -5,6 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -72,5 +76,21 @@ public class CustomFileUtil {
             }
         }
         return uploadNames;
+    }
+
+    public ResponseEntity<Resource> getFile(String fileName) {
+        FileSystemResource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+
+        if(!resource.isReadable()) {
+            resource = new FileSystemResource(uploadPath + File.separator + "default.jpeg");
+        }
+        HttpHeaders headers = new HttpHeaders();
+
+        try {
+            headers.add("Content-Type", Files.probeContentType(resource.getFile().toPath()));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok().headers(headers).body(resource);
     }
 }
