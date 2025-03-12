0905e1cc0eed589083fd97e5a209d905d9736020
YuYoHan <zxzz8014@naver.com>
2025-03-13 08:57:20 +0900

    [Feat] 상품 삭제하기 기능 구현

--- backend_mall/src/main/java/com/example/backend_mall/repository/ProductRepository.java
+++ backend_mall/src/main/java/com/example/backend_mall/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.example.backend_mall.repository;
 import com.example.backend_mall.entity.ProductEntity;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +13,8 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     @EntityGraph(attributePaths = "imageList")
     @Query("select p from ProductEntity p where p.pno = :pno")
     Optional<ProductEntity> selectOne(@Param("pno") Long pno);
+
+    @Modifying
+    @Query("update ProductEntity p set p.delFlag = :flag where p.pno = :pno")
+    void updateToDelete(@Param("pno") Long pno, @Param("flag") boolean flag);
 }
