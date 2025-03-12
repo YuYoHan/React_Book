99fef84ea4aed57674058dafa7b06c22fd337b8e
YuYoHan <zxzz8014@naver.com>
2025-03-13 07:30:49 +0900

    [Feat] 업로드 파일 삭제하기 기능 구현

--- backend_mall/src/main/java/com/example/backend_mall/config/file/CustomFileUtil.java
+++ backend_mall/src/main/java/com/example/backend_mall/config/file/CustomFileUtil.java
@@ -93,4 +93,24 @@ public class CustomFileUtil {
         }
         return ResponseEntity.ok().headers(headers).body(resource);
     }
+
+    public void deleteFiles(List<String> fileNames) {
+        if(fileNames == null || fileNames.size() == 0) {
+            return;
+        }
+
+        fileNames.forEach(fileName -> {
+            // 썸네일이 있는지 확인하고 삭제
+            String thumbnailFileName = "s_" + fileName;
+            Path thumbnailPath = Paths.get(uploadPath, thumbnailFileName);
+            Path filePath = Paths.get(uploadPath, fileName);
+
+            try {
+                Files.deleteIfExists(filePath);
+                Files.deleteIfExists(thumbnailPath);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 }
