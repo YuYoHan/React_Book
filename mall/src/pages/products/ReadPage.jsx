099f99f273390fc9f5662b7822152605ac596dee
YuYoHan <zxzz8014@naver.com>
2025-03-19 19:54:38 +0900

    [Add] products:ReadPage 추가

---
+++ mall/src/pages/products/ReadPage.jsx
@@ -0,0 +1,8 @@
+const ReadPage = () => {
+    return (
+        <div className="p-4 w-full bg-white">
+            <div className="text-3xl font-extrabold">Products Read Page</div>
+        </div>
+    );
+};
+export default ReadPage;
