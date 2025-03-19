bda60432af24e6dbfc2ae462b3d8fc7d9f50cfba
YuYoHan <zxzz8014@naver.com>
2025-03-19 19:50:21 +0900

    [Add] products:moveToRead 기능 구현

--- mall/src/components/products/ListComponent.jsx
+++ mall/src/components/products/ListComponent.jsx
@@ -5,6 +5,9 @@ import { useEffect, useState } from "react";
 import { getList } from "../../api/todoApi";
 import useCustomMove from "../../hooks/useCustomMove";
 import FetchingModal from "../common/FetchingModal";
+import { API_SERVER_HOST } from "../../api/todoApi";
+
+const host = API_SERVER_HOST;
 
 const initState = {
     dtoList: [],
@@ -38,8 +41,39 @@ const ListComponent = () => {
     return (
         <>
             <div className="border-2 border-blue-100 mt-10 mr-2 ml-2">
-                <h1>Products List Component</h1>
                 {fetching ? <FetchingModal /> : <></>}
+                <div className="flex flex-wrap mx-auto p-6">
+                    {serverData.dtoList.map((product) => (
+                        <div
+                            key={product.pno}
+                            className="w-1/2 p-1 rounded shadow-md border-2"
+                            onClick={() => moveToRead(product.pno)}
+                        >
+                            <div className="flex flex-col h-full">
+                                <div className="font-extrabold text-2xl p-2 w-full">
+                                    {product.pno}
+                                </div>
+                                <div className="text-1xl m-1 p-2 w-full flex flex-col">
+                                    <div className="w-full overflow-hidden">
+                                        <img
+                                            alt="product"
+                                            className="m-auto rounded-md w-60"
+                                            src={`${host}/api/products/view/s_${product.uploadFileNames[0]}`}
+                                        />
+                                    </div>
+                                    <div className="bottom-0 font-extrabold bg-white">
+                                        <div className="text-center p-1">
+                                            이름 : {product.pName}
+                                        </div>
+                                        <div className="text-center p-1">
+                                            가격 : {product.price}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     );
