2e1fad0b5806c16cd64a0aef77075f926b97cc88
YuYoHan <zxzz8014@naver.com>
2025-03-19 22:38:55 +0900

    [Add] ReadComponent에서 버튼 추가

--- mall/src/components/products/ReadComponent.jsx
+++ mall/src/components/products/ReadComponent.jsx
@@ -39,6 +39,56 @@ const ReadComponent = ({ pno }) => {
                     </div>
                 </div>
             </div>
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">PNAME</div>
+                    <div className="w-4/6 p-6 rounded-r border border-solid shadow-md">
+                        {product.pName}
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">PRICE</div>
+                    <div className="w-4/6 p-6 rounded-r border border-solid shadow-md">
+                        {product.price}
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">PDESC</div>
+                    <div className="w-4/6 p-6 rounded-r border border-solid shadow-md">
+                        {product.pDesc}
+                    </div>
+                </div>
+            </div>
+            <div className="w-full justify-center flex flex-col m-auto items-center">
+                {product.uploadFileNames.map((imagFile, i) => (
+                    <img
+                        alt="product"
+                        key={i}
+                        className="p-4 w-1/2"
+                        src={`${host}/api/products/view/${imagFile}`}
+                    />
+                ))}
+            </div>
+            <div className="flex justify-end p-4">
+                <button
+                    type="button"
+                    className="inline-block rounded p-4 m-2 text-xl w-32 text-white bg-red-500"
+                    onClick={() => moveToModify(pno)}
+                >
+                    Modify
+                </button>
+                <button
+                    type="button"
+                    className="rounded p-4 m-2 text-xl w-32 text-white bg-blue-500"
+                    onClick={moveToList}
+                >
+                    List
+                </button>
+            </div>
         </div>
     );
 };
