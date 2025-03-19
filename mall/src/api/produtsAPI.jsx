d1b3f97d762654028a109398a130e6cd6fda69a7
YuYoHan <zxzz8014@naver.com>
2025-03-19 19:24:41 +0900

    [Modify] produtAPI 수정

--- mall/src/api/produtsAPI.jsx
+++ mall/src/api/produtsAPI.jsx
@@ -9,3 +9,11 @@ export const postAdd = async (product) => {
     const res = await axios.post(`${host}/`, product, header);
     return res.data;
 };
+
+export const getList = async (pageParam) => {
+    const { page, size } = pageParam;
+    const res = await axios.get(`${host}/list`, {
+        params: { page: page, size: size },
+    });
+    return res.data;
+};
