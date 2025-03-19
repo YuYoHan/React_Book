621dfd900d25ea20570fd7a82f7a85b1f9b42a51
YuYoHan <zxzz8014@naver.com>
2025-03-19 22:40:53 +0900

    [Add] ModifyPage 추가

---
+++ mall/src/pages/products/ModifyPage.jsx
@@ -0,0 +1,9 @@
+const ModifyPage = () => {
+    return (
+        <div className="p-4 w-full bg-white">
+            <div className="text-3xl font-extrabold">Products Modify Page</div>
+        </div>
+    );
+};
+
+export default ModifyPage;
