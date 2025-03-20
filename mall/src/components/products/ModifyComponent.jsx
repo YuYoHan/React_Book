7eadeacf82c2c6e6d4587de9158ef9112d475390
YuYoHan <zxzz8014@naver.com>
2025-03-20 10:24:12 +0900

    [Add] ModifyComponent 추가

---
+++ mall/src/components/products/ModifyComponent.jsx
@@ -0,0 +1,9 @@
+const ModifyComponent = ({ pno }) => {
+    return (
+        <div className="border-2 border-sky-200 mt-10 m-2 p-4">
+            Product Modify Component
+        </div>
+    );
+};
+
+export default ModifyComponent;
