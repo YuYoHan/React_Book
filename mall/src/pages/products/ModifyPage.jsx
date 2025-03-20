f2f7e74a8af82f598e011fa0d88ed889c412f709
YuYoHan <zxzz8014@naver.com>
2025-03-20 10:25:56 +0900

    [Modify] ModifyPage에 ModifyComponent 추가

--- mall/src/pages/products/ModifyPage.jsx
+++ mall/src/pages/products/ModifyPage.jsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
+import ModifyComponent from "../../components/products/ModifyComponent";
+
 const ModifyPage = () => {
+    const { pno } = useParams();
     return (
         <div className="p-4 w-full bg-white">
             <div className="text-3xl font-extrabold">Products Modify Page</div>
+            <ModifyComponent pno={pno} />
         </div>
     );
 };
