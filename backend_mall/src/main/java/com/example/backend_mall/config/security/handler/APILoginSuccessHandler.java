a5042b8148a734e7eefd105c1e4db8a1460784ac
YuYoHan <zxzz8014@naver.com>
2025-03-30 19:35:57 +0900

    [Modify] APILoginSuccessHandler 수정

--- backend_mall/src/main/java/com/example/backend_mall/config/security/handler/APILoginSuccessHandler.java
+++ backend_mall/src/main/java/com/example/backend_mall/config/security/handler/APILoginSuccessHandler.java
@@ -5,6 +5,8 @@ package com.example.backend_mall.config.security.handler;
 *   작업이 가능합니다. API 서버의 경우 로그인 후에는 JSON 문자열을 생성해서 전달하도록 구성
 * */
 
+import com.example.backend_mall.dto.MemberDTO;
+import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,6 +15,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
 
 @Slf4j
 public class APILoginSuccessHandler implements AuthenticationSuccessHandler {
@@ -21,5 +25,15 @@ public class APILoginSuccessHandler implements AuthenticationSuccessHandler {
                                         HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
         log.debug("authentication {}", authentication);
+        MemberDTO memberDTO = (MemberDTO) authentication.getPrincipal();
+        Map<String, Object> claims = memberDTO.getClaims();
+        claims.put("accessToken", "");
+        claims.put("refreshToken", "");
+        Gson gson = new Gson();
+        String jsonStr = gson.toJson(claims);
+        response.setContentType("application/json; charset=URF-8");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println(jsonStr);
+        printWriter.close();
     }
 }
