54d86b4657eae2702c603d8f332f34e7328f76fe
YuYoHan <zxzz8014@naver.com>
2025-03-30 21:20:19 +0900

    [Add] APILoginFailHandler 추가

---
+++ backend_mall/src/main/java/com/example/backend_mall/config/security/handler/APILoginFailHandler.java
@@ -0,0 +1,26 @@
+package com.example.backend_mall.config.security.handler;
+
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+@Slf4j
+public class APILoginFailHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        Gson gson = new Gson();
+        String jsonStr = gson.toJson(Map.of("error", "ERROR_LOGIN"));
+        response.setContentType("application/json");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println(jsonStr);
+        printWriter.close();
+    }
+}
