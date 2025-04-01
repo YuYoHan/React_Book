b6768d8ef969af87fb19dc91c18a9b819f08fc7e
YuYoHan <zxzz8014@naver.com>
2025-04-01 12:26:24 +0900

    [Add] slice & reducer 추가

--- mall/src/components/menus/BasicMenus.jsx
+++ mall/src/components/menus/BasicMenus.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const BasicMenu = () => {
+    const loginState = useSelector((state) => state.loginSlice);
     return (
         <nav id="navBar" className=" flex bg-blue-300">
             <div className="w-4/5 bg-gray-500">
@@ -11,12 +13,19 @@ const BasicMenu = () => {
                     <li className="pr-6 text-2xl">
                         <Link to={"/about"}>About</Link>
                     </li>
-                    <li className="pr-6 text-2xl">
-                        <Link to={"/todo/"}>Todo</Link>
-                    </li>
-                    <li className="pr-6 text-2xl">
-                        <Link to={"/products/"}>Products</Link>
-                    </li>
+                    {/* 로그인한 사용자만 출력되는 화면 */}
+                    {loginState.email ? (
+                        <>
+                            <li className="pr-6 text-2xl">
+                                <Link to={"/todo/"}>Todo</Link>
+                            </li>
+                            <li className="pr-6 text-2xl">
+                                <Link to={"/products/"}>Products</Link>
+                            </li>
+                        </>
+                    ) : (
+                        <></>
+                    )}
                 </ul>
             </div>
             <div className="w-1/5 flex justify-end bg-orange-300 p-4 font-medium">
--- mall/src/store.jsx
+++ mall/src/store.jsx
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import loginSlice from "./slices/loginSlice";
 
 // Store 객체를 생성하기 위한 함수로 리덕스 툴킷의 configureStore()를 사용
 export default configureStore({
-    reducer: {},
+    reducer: {
+        loginSlice: loginSlice,
+    },
 });
