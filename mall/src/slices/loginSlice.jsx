600e95ed093ecb0d32748ad923ab238442d62a8b
YuYoHan <zxzz8014@naver.com>
2025-04-02 09:48:40 +0900

    [Modify] loginSlice에 쿠키설정 추가

--- mall/src/slices/loginSlice.jsx
+++ mall/src/slices/loginSlice.jsx
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { loginPost } from "../api/memberAPI";
+import { setCookie } from "../util/cookieUtil";
 
 const initState = {
     email: "",
@@ -39,6 +40,10 @@ const loginSlice = createSlice({
             .addCase(loginPostAsync.fulfilled, (state, action) => {
                 console.log("fulfilled");
                 const payload = action.payload;
+
+                if (!payload.error) {
+                    setCookie("member", JSON.stringify(payload), 1);
+                }
                 return payload;
             })
             .addCase(loginPostAsync.pending, (state, action) => {
