3f3e95ea01c7ad25c8a8b9105a3526d4c42ba672
YuYoHan <zxzz8014@naver.com>
2025-04-04 22:38:13 +0900

    [Feat] 장바구니 상품 조회 기능 구현

--- backend_mall/src/main/java/com/example/backend_mall/dto/CartItemListDTO.java
+++ backend_mall/src/main/java/com/example/backend_mall/dto/CartItemListDTO.java
@@ -9,7 +9,7 @@ public class CartItemListDTO {
     private Long cino;
     private int qty;
     private Long pno;
-    private String pname;
+    private String pName;
     private int price;
     private String imageFile;
 
@@ -18,13 +18,13 @@ public class CartItemListDTO {
     public CartItemListDTO(Long cino,
                            int qty,
                            Long pno,
-                           String pname,
+                           String pName,
                            int price,
                            String imageFile) {
         this.cino = cino;
         this.qty = qty;
         this.pno = pno;
-        this.pname = pname;
+        this.pName = pName;
         this.price = price;
         this.imageFile = imageFile;
     }
