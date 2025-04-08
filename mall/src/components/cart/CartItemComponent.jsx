0994a01c1c87a748bcc6e3ce3cdcd3273ee4467c
YuYoHan <zxzz8014@naver.com>
2025-04-08 11:18:16 +0900

    [Modify] CartItemComponent 수정

--- mall/src/components/cart/CartItemComponent.jsx
+++ mall/src/components/cart/CartItemComponent.jsx
@@ -1,10 +1,54 @@
+import { API_SERVER_HOST } from "../../api/todoApi";
+const host = API_SERVER_HOST;
+
 const CartItemComponent = ({ cino, pName, price, pno, qty, imageFile }) => {
+    const handleClickQty = (amount) => {};
+
     return (
-        <>
-            <div>
-                {cino} -- {pName}
+        <li key={cino} className="border-2">
+            <div className="w-full border-2">
+                <div className="m-1 p-1">
+                    <img src={`${host}/api/products/view/s_${imageFile}`} />
+                </div>
+                <div className="justify-center p-2 text-xl">
+                    <div className="justify-end w-full"></div>
+                    <div>Cart Item No: {cino}</div>
+                    <div>Pno: {pno}</div>
+                    <div>Name: {pName}</div>
+                    <div>Price: {price}</div>
+                    <div className="flex">
+                        <div className="w-2/3">Qty: {qty}</div>
+                        <div>
+                            <button
+                                className="m-1 p-1 text-2xl bg-orange-500 w-8 rounded-lg"
+                                onClick={() => handleClickQty(1)}
+                            >
+                                +
+                            </button>
+                            <button
+                                className="m-1 p-1 text-2xl bg-orange-500 w-8 rounded-lg"
+                                onClick={() => handleClickQty(-1)}
+                            >
+                                -
+                            </button>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="flex text-white font-bold p-2 justify-center">
+                            <button
+                                className="m-1 p-1 text-xl text-white bg-red-500 w-8 rounded-lg"
+                                onClick={() => handleClickQty(-1 * qty)}
+                            >
+                                X
+                            </button>
+                        </div>
+                        <div className="font-extrabold border-t-2 text-right m-2 pr-4">
+                            {qty * price}원
+                        </div>
+                    </div>
+                </div>
             </div>
-        </>
+        </li>
     );
 };
 
---
+++ mall/src/components/menus/CartComponent.jsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import useCustomCart from "../../hooks/useCustomCart";
+import useCustomLogin from "../../hooks/useCustomLogin";
+import CartItemComponent from "../cart/CartItemComponent";
+
+const CartComponent = () => {
+    const { isLogin, loginState } = useCustomLogin();
+    const { refreshCart, cartItems } = useCustomCart();
+
+    useEffect(() => {
+        if (isLogin) {
+            refreshCart();
+        }
+    }, [isLogin]);
+
+    return (
+        <div className="w-full">
+            {isLogin ? (
+                <div className="flex flex-col">
+                    <div className="w-full flex">
+                        <div className="font-extrabold text-2xl w-4/5">
+                            {loginState.nickName}'s Cart
+                        </div>
+                        <div className="bg-orange-600 text-center text-white font-bold w-1/5 rounded-full m-1">
+                            {cartItems.length}
+                        </div>
+                    </div>
+                    <div>
+                        <ul>
+                            {cartItems.map((item) => (
+                                <CartItemComponent {...item} key={item.cino} />
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            ) : (
+                <></>
+            )}
+        </div>
+    );
+};
+
+export default CartComponent;
