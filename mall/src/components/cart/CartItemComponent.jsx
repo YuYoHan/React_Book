f555ab2b9e3536230a8e2a9f2c05d2af3546ee43
YuYoHan <zxzz8014@naver.com>
2025-04-08 10:58:14 +0900

    [Add] CartItemComponent 추가

---
+++ mall/src/components/cart/CartItemComponent.jsx
@@ -0,0 +1,11 @@
+const CartItemComponent = ({ cino, pName, price, pno, qty, imageFile }) => {
+    return (
+        <>
+            <div>
+                {cino} -- {pName}
+            </div>
+        </>
+    );
+};
+
+export default CartItemComponent;
