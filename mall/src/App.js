2fb7043ec3844bf678d942828c5ddb52f4aab2fe
YuYoHan <zxzz8014@naver.com>
2025-04-08 13:13:25 +0900

    [Modify] useQuery() 사용

--- mall/src/App.js
+++ mall/src/App.js
@@ -1,14 +1,18 @@
-import './App.css';
-import './index.css';
-import { RouterProvider } from 'react-router-dom';
+import "./App.css";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
 import root from "./router/root";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient();
 function App() {
-  return (
-    <>
-      <RouterProvider router={root}/>
-    </>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={root} />
+            <ReactQueryDevtools initialIsOpen={true} />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
