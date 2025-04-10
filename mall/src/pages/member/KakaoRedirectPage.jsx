0ba5fba7ade9f8da5f40932ee15d85589e5ff779
YuYoHan <zxzz8014@naver.com>
2025-04-10 20:47:42 +0900

    [Modify] KakaoRedirectPage 수정

--- mall/src/pages/member/KakaoRedirectPage.jsx
+++ mall/src/pages/member/KakaoRedirectPage.jsx
@@ -1,22 +1,20 @@
 import { useSearchParams } from "react-router-dom";
 import { useEffect } from "react";
 import { getAccessToken, getMemberWithAccessToken } from "../../api/kakaoAPI";
-import { useDispatch } from "react-redux";
 import { login } from "../../slices/loginSlice";
 import useCustomLogin from "../../hooks/useCustomLogin";
 
 const KakaoRedirectPage = () => {
     const [searchParams] = useSearchParams();
+    const { moveToPath, saveAsCookie } = useCustomLogin();
     const authCode = searchParams.get("code");
-    const dispatch = useDispatch();
-    const { moveToPath } = useCustomLogin();
 
     useEffect(() => {
         getAccessToken(authCode).then(
             (accessToken) => {
                 getMemberWithAccessToken(accessToken).then((memberInfo) => {
                     console.log(memberInfo);
-                    dispatch(login(memberInfo));
+                    saveAsCookie(memberInfo);
 
                     // 소셜회원이 아니라면
                     if (memberInfo && !memberInfo.social) {
